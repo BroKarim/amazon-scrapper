9d4f11535b9b5c1829f49ee043779c0737d40f2b
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-12-22 15:13:32 +0700

    update the ui

--- index.js
+++ index.js
@@ -1,27 +1,107 @@
-//deklarasikan variabel express dan mengimpor modul express, yang digunakan untuk membuat server web dengan mudah.
 const express = require('express');
-//impor modul request-promise, yang digunakan untuk membuat permintaan HTTP berbasis Promise.
 const request = require('request-promise');
-
-// Membuat instance aplikasi Express menggunakan fungsi express()
+const bodyParser = require('body-parser'); // Add this line to parse the request body
 const app = express();
-
-//Mendeklarasikan variabel PORT yang akan menentukan port server.
-//Jika nilai dari environment variable PORT sudah diatur, maka gunakan nilainya; jika tidak, gunakan port default 5000.
 const PORT = process.env.PORT || 5000;
-
 const apiKey = '719fe24c1fcd3c9ea563faf322d2ca28';
 const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
-//pastikan bahwa request akan di parse sebagai objek JSON
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser middleware
 
-//Ketika permintaan diterima, server akan mengirimkan pesan "welcome to amazon scrapper API." sebagai respons.
 app.get('/', (req, res) => {
-  res.send('welcome to amazon scrapper API.');
+  res.send(`
+  <html>
+  <head>
+  <style>
+
+  body{
+    background-color: #121212;
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-family: 'Lato', sans-serif;
+  }
+
+    .container {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      text-align: center;
+      width: 50%;
+  }
+
+  .label {
+    margin: 0 auto;
+    text-align: center;
+    padding: 10px;       
+    background-color: #c0ffdd;
+    color: #000;
+}
+   
+    h2 {
+      font-family: 'Arial', sans-serif;
+      font-size: 3.75em;
+      margin: 5px;
+    }
+
+    .form {
+      margin-top: 5px;
+    }
+
+    .form label {
+      display: block;
+      margin-bottom: 5px;
+    }
+
+    .form input {
+      padding: 5px;
+      width: 200px;
+    }
+
+    .form button {
+      padding: 8px;
+      background-color: transparent;
+    
+      border-width: 5px;
+      
+      border-style: solid;
+      border-color: rgb(255, 0, 0);
+
+      color: white;
+      border: none;
+      cursor: pointer;
+    }
+  </style>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+</head>
+      <body>
+      <div class="container">
+        <div class="label">Web Scrapping</div>
+        <h2>Welcome to Amazon Scraper API</h2>
+        <p> Programmatically retrieve and present detailed information such as names, product details, and images from amazon </p>
+        <form class="form" action="/generate-link" method="post">
+          <label for="productCode">Enter Product Code:</label>
+          <input type="text" id="productCode" name="productCode" required>
+          <button  type="submit">Go</button>
+        </form>
+        </div>
+      </body>
+    </html>
+  `);
 });
 
-// GET PRODUCT DETAILS : http://localhost:5000/products/B0B8G8Y9QP
+app.post('/generate-link', (req, res) => {
+  const productCode = req.body.productCode;
+  const productLink = `http://localhost:5000/products/${productCode}`;
+  res.redirect(productLink);
+});
+
 app.get('/products/:productId', async (req, res) => {
   const { productId } = req.params;
 
@@ -32,54 +112,6 @@ app.get('/products/:productId', async (req, res) => {
     res.json(error);
   }
 });
-// GET PRODUCT Reviews : http://localhost:5000/products/B0B8G8Y9QP/reviews
-app.get('/products/:productId/reviews', async (req, res) => {
-  const { productId } = req.params;
 
-  try {
-    const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
-    res.json(JSON.parse(response));
-  } catch (error) {
-    res.json(error);
-  }
-});
-// GET PRODUCT offer : http://localhost:5000/products/B0B8G8Y9QP/offers
-app.get('/products/:productId/offers', async (req, res) => {
-  const { productId } = req.params;
 
-  try {
-    const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offers/${productId}`);
-    res.json(JSON.parse(response));
-  } catch (error) {
-    res.json(error);
-  }
-});
-// GET search result : http://localhost:5000/search/macbook
-app.get('/search/:searchQuery', async (req, res) => {
-  const { searchQuery } = req.params;
-
-  try {
-    const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
-    res.json(JSON.parse(response));
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//Menjalankan server pada port yang telah ditentukan. Ketika server berhasil dimulai, pesan "Server running on port {PORT}" akan ditampilkan di konsol.
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-// KONTEN
-/**
-
-- Kayaknya untuk yang sifatnya baru kita pelajari, dari pada ndak ada waktu kita pahami mending konten kita manfaatin untuk memahami
-- Sehingga tak bakal ada kontribusi leih pada penjelasan kode di ig
-Refernsi : 
-https://www.scrapehero.com/tutorial-how-to-scrape-amazon-product-details-using-python-and-selectorlib/
-
-- Nati card arahkan ke 
-
-
-
- */
